45d897ad38ac12cc0ae350c37e7f406ae53769dc
Tracy Wei <tracywei27@gmail.com>
2023-11-07 17:57:04 -0800

    updated assets

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -9,7 +9,7 @@ function Hero() {
         className="relative heroElem w-full pt-20 pb-40 m-auto flex justify-center text-center flex-col items-center z-1"
         style={{ maxWidth: "1200px" }}
       >
-        <img src="/static/logos/Headshot-min.jpg"
+        <img src="/static/logos/Headshot-min.JPG"
             className="rounded-full w-44 h-44"></img><br/>
         <h1 className="heroTitle inline-block max-w-2xl lg:max-w-4xl  w-auto relative text-5xl md:text-6xl lg:text-7xl tracking-tighter mb-10 font-bold heroShinyBg">
         👋 Hi, I'm <span className="heroShiny1 text-fun-pink">Tracy</span>!{" "}
